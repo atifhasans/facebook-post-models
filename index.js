df7d26b6086912dd12b8929f49bc3a854485f2b9
atifhasan <atifhasansiddiqi@gmail.com>
2025-01-06 10:19:13 +0500

    updated

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const postRoutes = require('./routes/postRoutes');
+const postRoutes = require('./routes/postRoutes'); // Ensure the path is correct
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
+
+// Default route for the root URL
+app.get('/', (req, res) => {
+  res.send('Welcome to the Express.js API!');
+});
 
 // Routes
 app.use('/api/posts', postRoutes);
@@ -14,12 +19,12 @@ app.use('/api/posts', postRoutes);
 // MongoDB connection
 const MONGO_URI = 'mongodb+srv://atifobjects:mn14K2zHIoHALbrN@cluster0.s7rhz.mongodb.net/';
 mongoose
-  .connect(MONGO_URI)
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log(err));
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 // Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
